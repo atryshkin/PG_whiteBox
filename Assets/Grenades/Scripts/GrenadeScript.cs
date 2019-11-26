63c39ce5c2bbcb8897ac29bc35639fd3868a2699
Andrei Tryshkin <andreytryshkin@gmail.com>
2019-11-26 18:27:05 +0300

    Throwing grenade was applied to cheracter

--- Assets/Grenades/Scripts/GrenadeScript.cs
+++ Assets/Grenades/Scripts/GrenadeScript.cs
@@ -76,21 +76,3 @@ public class GrenadeScript : MonoBehaviour {
         return calculatedDamage;
     }
 }
-
-[CustomEditor(typeof(GrenadeScript))]
-public class GrenadeEditor : Editor
-{
-    GrenadeScript grenadeScript;
-    void OnEnable()
-    {
-        grenadeScript = (GrenadeScript)target;
-    }
-
-    public override void OnInspectorGUI()
-    {
-        base.OnInspectorGUI();
-        if(grenadeScript.grenadeType == GrenadeScript.GrenadeType.Chemical) {
-            grenadeScript.chemicalDamageDuration = EditorGUILayout.FloatField("Chemical Damage Duration:", grenadeScript.chemicalDamageDuration);
-        }
-    }
-}
